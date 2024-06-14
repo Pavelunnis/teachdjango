53da60b52bc357e2877c2d617bd96df1415da794
Pavelunnis <156346047+Pavelunnis@users.noreply.github.com>
2024-06-14 14:21:58 +0300

    Add files via upload

---
+++ hello/firstapp/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("<h2>Главная</h2>")
+
+
+def about(request):
+    return HttpResponse("<h2>О Сайте</h2>")
+
+
+def contact(request):
+    return HttpResponse("<h2>Контакты</h2>")
