74f1cb3164e8ca25b7f375953b9089f8364c6275
Pavelunnis <156346047+Pavelunnis@users.noreply.github.com>
2024-06-19 20:47:33 +0300

    каталог шаблонов(templates)

--- hello/firstapp/views.py
+++ hello/firstapp/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.http import *
-
+from django.template.response import TemplateResponse
 
 def index(request):
-    return HttpResponse("<h2>Главная</h2>")
+    return TemplateResponse(request, "firstapp/home.html")
 
 
 def about(request):
